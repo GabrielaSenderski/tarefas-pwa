707a51b78b5c134764bf725725f680e3aebb590d
GabrielaSenderski <gabrielasenderski.ifc@gmail.com>
2026-08-08 14:58:41 -0300

    feat: store de autenticação

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,8 @@
   <UpdatePrompt />
 
   <!-- comando para rodar o projeto no docker: -->
-<!--docker run -p 8001:8001 eduardosilvasc/gerenciamento-tarefas-2026:2.0 -->
+<!--docker run -p 8001:8001 eduardosilvasc/gerenciamento-tarefas-2026:3.0 -->
+<!-- http://localhost:8001/docs -->
   
 </template>
 
